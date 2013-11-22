360e918f69a62299cfbffb834b3bed0918ad0a07
venky91 <venksundram@gmail.com>
2013-11-21 20:08:46 -0800

    added init, start, stop, destroy and run methods to Driver

--- src/ultracefighter/Driver.java
+++ src/ultracefighter/Driver.java
@@ -1,6 +1,37 @@
 package ultracefighter;
 
-public class Driver {
+import java.applet.Applet;
+
+public class Driver extends Applet implements Runnable {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 
