8bcd1394f985e4d52c128f700a63fa524a7f924c
venky91 <venksundram@gmail.com>
2013-11-22 12:59:03 -0800

    added hero plane sprite to applet, can move around

--- src/ultracefighter/Driver.java
+++ src/ultracefighter/Driver.java
@@ -3,65 +3,159 @@ package ultracefighter;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 public class Driver extends Applet implements Runnable, KeyListener {
+
+	private Plane plane;
+	private Image image, hero;
+	private URL base;
+	private Graphics second;
 
 	@Override
 	public void init() {
 		setSize(800, 600);
-		setBackground(Color.BLACK);
+		setBackground(Color.WHITE);
 		setFocusable(true);
-		
+
 		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Ultra Ace Fighter");
+
+		try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+
+		}
+
+		// Image setups
+		hero = getImage(base, "data/F-22B.png");
 	}
-	
+
 	@Override
 	public void start() {
-		
-		Thread thread = new Thread();
+
+		plane = new Plane();
+		Thread thread = new Thread(this);
 		thread.start();
-		
+
 	}
-	
+
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (true) {
+			plane.update();
+
+			repaint();
+
+			try {
+
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(hero, plane.getCenterX() - 40, plane.getCenterY() - 59,
+				this);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+
+		case KeyEvent.VK_UP:
+			plane.moveUp();
+			plane.setMovingUp(true);
+			break;
+
+		case KeyEvent.VK_DOWN:
+			plane.moveDown();
+			plane.setMovingDown(true);
+			break;
+
+		case KeyEvent.VK_LEFT:
+			plane.moveLeft();
+			plane.setMovingLeft(true);
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			plane.moveRight();
+			plane.setMovingRight(true);
+			break;
+
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+
+		case KeyEvent.VK_UP:
+			plane.stopUp();
+			break;
+
+		case KeyEvent.VK_DOWN:
+			plane.stopDown();
+			break;
+
+		case KeyEvent.VK_LEFT:
+			plane.stopLeft();
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			plane.stopRight();
+			break;
+
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 }
--- src/ultracefighter/Plane.java
+++ src/ultracefighter/Plane.java
@@ -1,9 +1,159 @@
 package ultracefighter;
 
 public class Plane {
+	
+	final int MOVESPEED = 5;
 
 	private int centerX = 400;
-	private int centerY = 582;
+	private int centerY = 482;
+	
+	private int speedX = 0;
+	private int speedY = 0;
+	
+	private boolean movingLeft = false;
+    private boolean movingRight = false;
+    private boolean movingUp = false;
+    private boolean movingDown = false;
+	
+	public void update() {
+		
+		centerX += speedX;
+		centerY += speedY;
+		
+		if (centerX <= 39 ) {
+			centerX = 40;
+		}
+		
+		if (centerX >= 760) {
+			centerX = 759;
+		}
+		
+		if (centerY >= 542) {
+			centerY = 541;
+		}
+		
+		if (centerY <= 58) {
+			centerY = 59;
+		}
+	
+	}
+	
+	public void moveUp() {
+		speedY = -MOVESPEED;
+	}
+	
+	public void moveDown() {
+		speedY = MOVESPEED;
+	}
+	
+	public void stopUp() {
+		setMovingUp(false);
+		stopY();
+	}
+	
+	public void stopDown() {
+		setMovingDown(false);
+		stopY();
+	}
+	
+	public void stopY() {
+		if (isMovingUp() == false && isMovingDown() == false) {
+			speedY = 0;
+		}
+		else if (isMovingUp() == false && isMovingDown() == true) {
+			moveDown();
+		}
+		else {
+			moveUp();
+		}
+	}
+	
+	public void moveLeft() {
+		speedX = -MOVESPEED;
+	}
+	
+	public void moveRight() {
+		speedX = MOVESPEED;
+	}
+	
+	public void stopLeft() {
+		setMovingLeft(false);
+		stopX();
+	}
+	
+	public void stopRight() {
+		setMovingRight(false);
+		stopX();
+	}
+	
+	public void stopX() {
+		if (isMovingRight() == false && isMovingLeft() == false) {
+			speedX = 0;
+		}
+		else if (isMovingRight() == false && isMovingLeft() == true) {
+			moveLeft();
+		}
+		else {
+			moveRight();
+		}
+	}
+	
+	
+	// Getters and Setters
+	public int getCenterX() {
+		return centerX;
+	}
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+	public int getCenterY() {
+		return centerY;
+	}
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+	public int getSpeedX() {
+		return speedX;
+	}
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+	public int getSpeedY() {
+		return speedY;
+	}
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+	public int getMOVESPEED() {
+		return MOVESPEED;
+	}
+	public boolean isMovingLeft() {
+		return movingLeft;
+	}
+	public void setMovingLeft(boolean movingLeft) {
+		this.movingLeft = movingLeft;
+	}
+	public boolean isMovingRight() {
+		return movingRight;
+	}
+	public void setMovingRight(boolean movingRight) {
+		this.movingRight = movingRight;
+	}
+	public boolean isMovingUp() {
+		return movingUp;
+	}
+	public void setMovingUp(boolean movingUp) {
+		this.movingUp = movingUp;
+	}
+	public boolean isMovingDown() {
+		return movingDown;
+	}
+	public void setMovingDown(boolean movingDown) {
+		this.movingDown = movingDown;
+	}
+	
+	
 
 
+	
 }
