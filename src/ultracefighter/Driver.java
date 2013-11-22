5312f2ccb48a570a0da7a706b6606c0f890b4260
venky91 <venksundram@gmail.com>
2013-11-21 20:11:29 -0800

    added frame in Driver

--- src/ultracefighter/Driver.java
+++ src/ultracefighter/Driver.java
@@ -1,13 +1,22 @@
 package ultracefighter;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Driver extends Applet implements Runnable {
+public class Driver extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
 		
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Ultra Ace Fighter");
 	}
 	
 	@Override
@@ -29,6 +38,24 @@ public class Driver extends Applet implements Runnable {
 	}
 	@Override
 	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
